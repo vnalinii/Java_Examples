6d27ae12f3b4044a2c3fd4f04dc736be7aafc5cb
rheer <rheer@DESKTOP-845EPTU.Dlink>
2020-05-01 20:30:41 +0530

    commit

---
+++ Helloworld/com/java/heera/Helloworld.java
@@ -0,0 +1,11 @@
+package com.java.heera;
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hellooo..");
+		System.out.println("Welcome to Java Programming");
+		int a=5,b=7;
+		int c= a+b;
+		System.out.println(c);
+	}
+}
