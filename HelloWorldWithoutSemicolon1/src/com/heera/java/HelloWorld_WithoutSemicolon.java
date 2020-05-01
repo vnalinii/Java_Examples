8ac2213d366d31364c5bc9bdb4c714842b0c0bdc
rheer <rheer@DESKTOP-845EPTU.Dlink>
2020-05-01 23:57:14 +0530

    code commit

---
+++ HelloWorldWithoutSemicolon1/src/com/heera/java/HelloWorld_WithoutSemicolon.java
@@ -0,0 +1,14 @@
+package com.heera.java;
+public class HelloWorld_WithoutSemicolon {
+
+	public static void main(String[] args) {
+		
+		
+		if(System.out.printf("HelloWorld")==null)
+		{
+			
+		}
+
+	}
+
+}
