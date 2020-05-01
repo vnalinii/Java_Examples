9e46bec41bef4e1972e94b40dbaa14cf89e13c9d
rheer <rheer@DESKTOP-845EPTU.Dlink>
2020-05-01 23:46:10 +0530

    code commit

---
+++ BiggestNumber/src/com/heera/java/BiggestNumber.java
@@ -0,0 +1,17 @@
+package com.heera.java;
+
+public class BiggestNumber 
+{
+	public static void main(String[] args) 
+	{
+		int a = 40;
+		int b = 35;
+		int c = 25;
+		if (a > b && a > c)
+			System.out.println("A is Biggest Number");
+		else if (b > c)
+			System.out.println("B is the Biggest Number");
+		else
+			System.out.println("C is the Biggest Number");
+	}
+}
