3f27f955f27acb193b4dfa7d11782472dcc4bc1b
rheer <rheer@DESKTOP-845EPTU.Dlink>
2020-05-02 21:03:18 +0530

    code commit

---
+++ DuplicateNumber/src/com/heera/java/DuplicateNumber.java
@@ -0,0 +1,37 @@
+package com.heera.java;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicateNumber{ 
+	
+	    private static String highestNumber;
+
+		public int findDuplicateNumber(List<Integer> numbers){
+	         
+	        int highestNumber = numbers.size() - 1;
+	        int total = getSum(numbers);
+	        int duplicate = total - (highestNumber*(highestNumber+1)/2);
+	        return duplicate;
+	    }
+	     
+	    public int getSum(List<Integer> numbers){
+	         
+	        int sum = 0;
+	        for(int num:numbers){
+	            sum += num;
+	        }
+	        return sum;
+	    }
+	     
+	    public static void main(String args[]){
+	        List<Integer> numbers = new ArrayList<Integer>();
+	        for(int i=1;i<30;i++){
+	            numbers.add(i);
+	        }
+	        //add duplicate number into the list
+	        numbers.add(24);
+	        DuplicateNumber dn = new DuplicateNumber();
+	        System.out.println("Duplicate Number: "+dn.findDuplicateNumber(numbers));
+	       
+	    }
+	}
