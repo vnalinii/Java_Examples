7a62f103963734c8856f83f452091e28c99176ea
rheer <rheer@DESKTOP-845EPTU.Dlink>
2020-05-02 20:19:27 +0530

    code commit

---
+++ Number1to10/src/com/heera/java/Number1to10.java
@@ -0,0 +1,15 @@
+package com.heera.java;
+public class Number1to10 {
+
+	public static void main(String[] args) {
+		//for(int i=1;i<=10;i++) {
+        int i=1;
+		do {
+			//int i=1;	
+		System.out.println(i);
+		i++;
+		}
+		while(i<=10);
+		//}
+	}
+}
