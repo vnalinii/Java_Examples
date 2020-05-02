29caa977b7aafd2a3e9906ae61438ccc7a137a9f
rheer <rheer@DESKTOP-845EPTU.Dlink>
2020-05-02 20:27:20 +0530

    code commit

---
+++ StaticBlockSample/src/com/heera/java/StaticBlockSample.java
@@ -0,0 +1,12 @@
+// Static Block Example
+// Static Block executes before main() method
+package com.heera.java;
+public class StaticBlockSample {
+
+	static{
+		System.out.println("Static Block Sample!");	
+	}
+	public static void main(String[] args) {
+	}
+
+}
