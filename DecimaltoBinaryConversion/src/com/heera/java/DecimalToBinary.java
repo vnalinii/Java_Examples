fd0051683c183bda64518d478c720543fc54921b
rheer <rheer@DESKTOP-845EPTU.Dlink>
2020-05-02 21:01:57 +0530

    code commit

---
+++ DecimaltoBinaryConversion/src/com/heera/java/DecimalToBinary.java
@@ -0,0 +1,11 @@
+package com.heera.java;
+public class DecimalToBinary {
+
+	public static void main(String[] args) {
+		System.out.println("The Binary equivalent to Decimal 124 is : " + Integer.toBinaryString(124));
+		// System.out.println(Integer.toBinaryString(124));
+
+		System.out.println("The Binary equivalent to Decimal 234 is : " + Integer.toBinaryString(234));
+		// System.out.println(Integer.toBinaryString(234));
+	}
+}
