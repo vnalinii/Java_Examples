721faf3a357d44c22f0e536baea04e02eee5843f
vnalinii <65073519+vnalinii@users.noreply.github.com>
2020-05-10 15:10:41 +0530

    Update SamplePattern.java

--- BasicPatternProblems/src/com/heera/java/SamplePattern.java
+++ BasicPatternProblems/src/com/heera/java/SamplePattern.java
@@ -4,6 +4,7 @@ public class SamplePattern {
 
 	public static void main(String[] args) {
 		for(int i=0;i<=5;i++) {
+			//Nalini comment
 			for(int j=0;j<=i;j++) {
 				System.out.print(i);
 				//i++;
